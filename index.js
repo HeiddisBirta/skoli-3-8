d19a9d4e57d1aa7074a5198bc321f462ae12c41c
Heiddis Birta <156322797+HeiddisBirta@users.noreply.github.com>
2024-09-17 10:37:30 +0000

    made a typescript file and a javascript file

---
+++ index.js
@@ -0,0 +1,30 @@
+// Here is the javaScript file that was generated after I finished coding the index.ts file and wrote node .index.tx in the terminal
+
+var isCompleted = true;
+var isPending = false;
+
+var numberArray = [1, 2, 3, 4, 5];
+
+var booleanArray = [true, false, true, true];
+
+var num1 = 10;
+var num2 = 5;
+
+var sum = num1 + num2;
+var difference = num1 - num2;
+
+var firstName = "John";
+var lastName = "Doe";
+var fullName = firstName + " " + lastName;
+
+console.log("\nBoolean true or false Values:");
+console.log("Is completed? " + isCompleted);
+console.log("Is pending? " + isPending);
+console.log("\nArray Values:");
+console.log("Number Array: " + numberArray);
+console.log("Boolean Array: " + booleanArray);
+console.log("\nArithmetic Operations:");
+console.log("Sum: ".concat(sum));
+console.log("Difference: ".concat(difference));
+console.log("\nString Concatenation:");
+console.log("Full Name: " + fullName);
